8fa23a80b0763ff49e6f0d8f0fc3a0833033ec5a
GsusLara <jelara99@gmail.com>
2022-01-13 13:06:26 -0600

    restructura del header

--- layout/layout.js
+++ layout/layout.js
@@ -1,13 +1,11 @@
 import Navbar from "./navbar"
 import Footer from "./footer"
-import Menu from "./menu"
 
 export default function Layout(props) {
     const { children } = props
     return (
         <div className="container-fluid Pagina">
             <Navbar/>
-            <Menu/>
             <div className="container Contenido">
                 {children}
             </div>
--- layout/navbar/navbar.js
+++ layout/navbar/navbar.js
@@ -1,25 +1,63 @@
 import Image from 'next/image'
 import Link from "next/link"
 import logo from "../../public/logo.png"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 export default function Navbar() {
-    return (
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-            <div className="container">
-                <Link href="/">
-                    <a><Image src={logo} alt="logo" /></a>
-                </Link>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" />
-                <div className="collapse navbar-collapse ">
-                    <form className="d-flex ms-auto">
-                        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-                        <button className="btn btn-outline-success" type="submit">Search</button>
-                    </form>
-                </div>
-            </div>
-        </nav>
-    )
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container">
+        <div className="col-5 col-md-3 col-lg-2">
+          <Logo />
+        </div>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse ms-md-5" id="navbarSupportedContent">
+          <ul className="navbar-nav  mb-2 mb-lg-0 ms-md-auto">
+            <li className="nav-item">
+              <a
+                className="nav-link p-0 fs-4"
+                href="#">
+                <FontAwesomeIcon icon={["far", "user"]} />
+              </a>
+            </li>
+            <li className="nav-item">
+              <a 
+              className="nav-link p-0 fs-4 mx-3" 
+              href="#">
+                <FontAwesomeIcon icon={["far", "heart"]} />
+                </a>
+            </li>
+            <li className="nav-item">
+            <a 
+              className="nav-link p-0 fs-4" 
+              href="#">
+                <FontAwesomeIcon icon={["fas", "shopping-cart"]} />
+                </a>
+            </li>
+          </ul>
+          {/* <form className="d-flex">
+            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+            <button className="btn btn-outline-success" type="submit">Search</button>
+          </form> */}
+        </div>
+      </div>
+    </nav>
+  )
+}
+
+function Logo() {
+  return (
+    <Link href="/">
+      <a className="navbar-brand"><Image src={logo} alt="logo" /></a>
+    </Link>
+  )
 }
