acc941d1136f6418d7445739ebb9a1f27ac4651c
GsusLara <jelara99@gmail.com>
2022-02-08 11:54:19 -0600

    pruebas de autenticación

--- components/login/login.js
+++ components/login/login.js
@@ -1,7 +1,8 @@
 import { useState } from "react";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { auth } from "../../store/firebaseConfig"
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signInWithRedirect, GoogleAuthProvider } from "firebase/auth"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { auth } from "../../store/firebaseConfig";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signInWithRedirect, GoogleAuthProvider } from "firebase/auth";
+
 
 
 export default function Login() {
@@ -9,11 +10,27 @@ export default function Login() {
     const [conCuenta, setconCuenta] = useState(true);
     const [credentials, setcredentials] = useState({ email: "", password: "", password2: "" });
     const revisionEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
     const changeUser = (e) => {
         setcredentials({
             ...credentials,
             [e.target.name]: e.target.value
         })
+    }
+    const verificar = () => {
+        if (credentials.email =="" || credentials.password==""){
+            alert("complete todos los campos")
+        }else if (revisionEmail.test(credentials.email) !== true ) {
+            alert("ingrese un email válido")
+        }else if (credentials.password.length < 8){
+            alert("contraseña invalida")
+        } else if (conCuenta) {
+            iniciaUser()
+        } else if (credentials.password !== credentials.password2) {
+            alert("las contraseñas no coinciden")
+        } else {
+            registerUser();
+        }
     }
     const registerUser = async () => {
         try {
@@ -32,43 +49,28 @@ export default function Login() {
         }
     }
 
-    const verificar = () => {
-        if (revisionEmail.test(credentials.email) !== true || credentials.password.length < 8) {
-            alert("email o password invalido")
-        } else if (conCuenta) {
-            iniciaUser()
-        } else if (credentials.password !== credentials.password2) {
-            alert("las contraseñas no coinciden")
-        } else {
-            registerUser();
-        }
-    }
-
     return (
         <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
             <div className="modal-dialog">
                 <div className="modal-content bg-dark">
                     <div className="modal-header">
                         <h5 className="modal-title" id="exampleModalLabel">{conCuenta ? "Iniciar sesión" : "Crear cuenta"}</h5>
-                        <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        <button type="button" className="closeBtn" data-bs-dismiss="modal" aria-label="Close"><FontAwesomeIcon icon={["fas", "times"]} /></button>
                     </div>
                     <div className="modal-body text-center">
-                        <article className="card-body mx-auto" style={{ maxWidth: "400px" }}>
-                            
-                            {conCuenta ?
-                                <button className="btn btn-primary " onClick={() => signInWithRedirect(auth, googleProvider)}><img src="/google.ico" alt="inicio google" />oogle</button>:
-                                <button className="btn btn-danger" onClick={() => signInWithRedirect(auth, googleProvider)}> <FontAwesomeIcon icon={["fab", "google"]} /> &nbsp; Utiliza tu cuenta de Google</button>
-                            }
-                                <p className="my-3">ó</p>
+                        <div className="card-body mx-auto" style={{ maxWidth: "400px" }}>
+                            <button className="btn btn-primary " onClick={() => signInWithRedirect(auth, googleProvider)}><img src="/google.ico" alt="inicio google" />oogle</button>
+                            <p className="my-3">ó</p>
                             <div>
-                                <form >
+
+                                <form>
                                     <div className="input-group mb-3">
                                         <span className="input-group-text" id="basic-addon1"><FontAwesomeIcon icon={["fa", "envelope"]} /></span>
                                         <input
+                                            className="form-control"
                                             name="email"
                                             type="email"
                                             autoComplete="on"
-                                            className="form-control"
                                             placeholder="Email address"
                                             onChange={changeUser}
                                             onKeyPress={e => {
@@ -81,10 +83,10 @@ export default function Login() {
                                     <div className="input-group mb-3">
                                         <span className="input-group-text" id="basic-addon1"><FontAwesomeIcon icon={["fa", "lock"]} /></span>
                                         <input
+                                            className="form-control"
                                             name="password"
                                             type="password"
                                             autoComplete="on"
-                                            className="form-control"
                                             placeholder={conCuenta ? "password" : "contraseña superior a 8 digitos"}
                                             onChange={changeUser}
                                             onKeyPress={e => {
@@ -98,10 +100,10 @@ export default function Login() {
                                         <div className="input-group mb-3">
                                             <span className="input-group-text" id="basic-addon1"><FontAwesomeIcon icon={["fa", "lock"]} /></span>
                                             <input
+                                                className="form-control"
                                                 name="password2"
                                                 type="password"
                                                 autoComplete="on"
-                                                className="form-control"
                                                 placeholder="confirme su contraseña"
                                                 onChange={changeUser}
                                                 onKeyPress={e => {
@@ -123,11 +125,12 @@ export default function Login() {
                                     }
                                 </div>
                             </div>
-                        </article>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     )
 }
+
 
