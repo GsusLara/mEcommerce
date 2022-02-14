91422e0d48d006519d6a2bf36d34b65f9a11ef5c
GsusLara <jelara99@gmail.com>
2022-02-14 15:20:25 -0600

    cambio de  modal

--- components/login/login.js
+++ components/login/login.js
@@ -1,10 +1,11 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { auth } from "../../store/firebaseConfig";
 import { toast } from "react-toastify"
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signInWithRedirect, GoogleAuthProvider, onAuthStateChanged } from "firebase/auth";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signInWithRedirect, GoogleAuthProvider } from "firebase/auth";
 
-export default function Login() {
+export default function Login(props) {
+    const { handleClose } = props
     const googleProvider = new GoogleAuthProvider();
     const [conCuenta, setconCuenta] = useState(true);
     const [loading, setloading] = useState(false);
@@ -53,112 +54,99 @@ export default function Login() {
         }
         setloading(false)
     }
-    const iniciaGoogle = async()=>{
+    const iniciaGoogle = async () => {
         try {
             await signInWithRedirect(auth, googleProvider)
         } catch (error) {
-            console.log(error)
         }
     }
-    useEffect(() => {
-        const autenticando = onAuthStateChanged(auth, (usuarioFirebase) => {
-            console.log(usuarioFirebase)
-        })
-        return () => {
-            autenticando()
-        }
-    }, [])
 
     return (
-        <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div className="modal-dialog">
-                <div className="modal-content bg-dark">
-                    <div className="modal-header">
-                        <h5 className="modal-title" id="exampleModalLabel">{conCuenta ? "Iniciar sesión" : "Crear cuenta"}</h5>
-                        <button type="button" className="closeBtn" data-bs-dismiss="modal" aria-label="Close"><FontAwesomeIcon icon={["fas", "times"]} /></button>
-                    </div>
-                    <div className="modal-body text-center">
-                        <div className="card-body mx-auto" style={{ maxWidth: "400px" }}>
-                            <button className="btn btn-outline-primary" onClick={() => iniciaGoogle()}><img src="/google.ico" alt="inicio google" />oogle</button>
-                            <p className="linea my-3 fs-5"><span className="px-2 bg-dark">ó</span></p>
-                            <div>
-                                <form>
-                                    <div className="input-group mb-3">
-                                        <span className="input-group-text" id="basic-addon1"><FontAwesomeIcon icon={["fa", "envelope"]} /></span>
-                                        <input
-                                            className="form-control"
-                                            name="email"
-                                            type="email"
-                                            autoComplete="on"
-                                            placeholder="Correo electrónico"
-                                            onChange={changeUser}
-                                            onKeyPress={e => {
-                                                if (e.key == "Enter") {
-                                                    verificar();
-                                                }
-                                            }}
-                                        />
-                                    </div>
-                                    <div className="input-group">
-                                        <span className="input-group-text" id="basic-addon1"><FontAwesomeIcon icon={["fa", "lock"]} /></span>
-                                        <input
-                                            className="form-control"
-                                            name="password"
-                                            type="password"
-                                            autoComplete="on"
-                                            placeholder="contraseña"
-                                            onChange={changeUser}
-                                            onKeyPress={e => {
-                                                if (e.key == "Enter") {
-                                                    verificar();
-                                                }
-                                            }}
-                                        />
-                                    </div>
-                                    {!conCuenta &&
-                                        <>
-                                            <div id="passwordHelpBlock" className="form-text mb-2">
-                                                La contraseña debe ser superior a 8 caracteres
-                                            </div>
-                                            <div>
-                                                <div className="input-group">
-                                                    <span className="input-group-text" id="basic-addon1"><FontAwesomeIcon icon={["fa", "lock"]} /></span>
-                                                    <input
-                                                        className="form-control"
-                                                        name="password2"
-                                                        type="password"
-                                                        autoComplete="on"
-                                                        placeholder="confirme su contraseña"
-                                                        onChange={changeUser}
-                                                        onKeyPress={e => {
-                                                            if (e.key == "Enter") {
-                                                                verificar();
-                                                            }
-                                                        }}
-                                                    />
-                                                </div>
-                                            </div>
-                                        </>
-                                    }
-                                </form>
-                                <div>
-                                    <button
-                                        className="btn btn-primary mt-3 mb-3"
-                                        onClick={() => verificar()}>
-                                        {loading ?
-                                            <div className="spinner-border spinner-border-sm" role="status">
-                                                <span className="visually-hidden">Loading...</span>
-                                            </div>
-                                            : conCuenta ? "ingresar" : "Registrarme"}
-                                    </button>
-                                </div>
-                                <div className="modal-footer">
-                                    {conCuenta ?
-                                        <p>Registrate {" "}<a className="text-primary loginButton" onClick={() => setconCuenta(false)}>aqui</a> </p> :
-                                        <p>tienes cuenta? <a className="text-primary loginButton" onClick={() => setconCuenta(true)}>Iniciar sesión</a> </p>
-                                    }
-                                </div>
+        <div className="modal-content bg-dark">
+            <div className="modal-header">
+                <h5 className="modal-title" id="exampleModalLabel">{conCuenta ? "Iniciar sesión" : "Crear cuenta"}</h5>
+                <button type="button" className="closeBtn" onClick={handleClose}><FontAwesomeIcon icon={["fas", "times"]} /></button>
+            </div>
+            <div className="modal-body text-center">
+                <div className="card-body mx-auto" style={{ maxWidth: "400px" }}>
+                    <button className="btn btn-outline-primary" onClick={() => iniciaGoogle()}><img src="/google.ico" alt="inicio google" />oogle</button>
+                    <p className="linea my-3 fs-5"><span className="px-2 bg-dark">ó</span></p>
+                    <div>
+                        <form>
+                            <div className="input-group mb-3">
+                                <span className="input-group-text" id="basic-addon1"><FontAwesomeIcon icon={["fa", "envelope"]} /></span>
+                                <input
+                                    className="form-control"
+                                    name="email"
+                                    type="email"
+                                    autoComplete="on"
+                                    placeholder="Correo electrónico"
+                                    onChange={changeUser}
+                                    onKeyPress={e => {
+                                        if (e.key == "Enter") {
+                                            verificar();
+                                        }
+                                    }}
+                                />
                             </div>
+                            <div className="input-group">
+                                <span className="input-group-text" id="basic-addon1"><FontAwesomeIcon icon={["fa", "lock"]} /></span>
+                                <input
+                                    className="form-control"
+                                    name="password"
+                                    type="password"
+                                    autoComplete="on"
+                                    placeholder="contraseña"
+                                    onChange={changeUser}
+                                    onKeyPress={e => {
+                                        if (e.key == "Enter") {
+                                            verificar();
+                                        }
+                                    }}
+                                />
+                            </div>
+                            {!conCuenta &&
+                                <>
+                                    <div id="passwordHelpBlock" className="form-text mb-2">
+                                        La contraseña debe ser superior a 8 caracteres
+                                    </div>
+                                    <div>
+                                        <div className="input-group">
+                                            <span className="input-group-text" id="basic-addon1"><FontAwesomeIcon icon={["fa", "lock"]} /></span>
+                                            <input
+                                                className="form-control"
+                                                name="password2"
+                                                type="password"
+                                                autoComplete="on"
+                                                placeholder="confirme su contraseña"
+                                                onChange={changeUser}
+                                                onKeyPress={e => {
+                                                    if (e.key == "Enter") {
+                                                        verificar();
+                                                    }
+                                                }}
+                                            />
+                                        </div>
+                                    </div>
+                                </>
+                            }
+                        </form>
+                        <div>
+                            <button
+                                className="btn btn-primary mt-3 mb-3"
+                                onClick={() => verificar()}>
+                                {loading ?
+                                    <div className="spinner-border spinner-border-sm" role="status">
+                                        <span className="visually-hidden">Loading...</span>
+                                    </div>
+                                    : conCuenta ? "ingresar" : "Registrarme"}
+                            </button>
+                        </div>
+                        <div className="modal-footer">
+                            {conCuenta ?
+                                <p>Registrate {" "}<a className="text-primary loginButton" onClick={() => setconCuenta(false)}>aqui</a> </p> :
+                                <p>tienes cuenta? <a className="text-primary loginButton" onClick={() => setconCuenta(true)}>Iniciar sesión</a> </p>
+                            }
                         </div>
                     </div>
                 </div>
--- layout/navbar/navbar.js
+++ layout/navbar/navbar.js
@@ -1,18 +1,32 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import Modal from 'react-bootstrap/Modal'
 import Image from 'next/image'
 import Link from "next/link"
 import logo from "../../public/logo.png"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import Login from '../../components/login';
 import { auth } from "../../store/firebaseConfig";
-import { signOut } from "firebase/auth"
+import { signOut, onAuthStateChanged } from "firebase/auth"
 
 
 export default function Navbar() {
   const [btnMenu, setbtnMenu] = useState(false)
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
   const menuActivo = () => {
     setbtnMenu(!btnMenu);
   }
+  useEffect(() => {
+    const autenticando = onAuthStateChanged(auth, (usuarioFirebase) => {
+      console.log(usuarioFirebase)
+      handleClose
+    })
+    return () => {
+      autenticando()
+    }
+  }, [])
+
   return (
     <nav className="navbar navbar-expand-lg navbar-light">
       <div className="container">
@@ -39,12 +53,11 @@ export default function Navbar() {
             <button className="btnbuscar btn fs-5" type="submit"> <FontAwesomeIcon icon={["fas", "search"]} /></button>
           </form>
           <ul className="navbar-nav  mb-2 mb-lg-0 ms-md-auto">
-            <li className="nav-item"><a className="nav-link" onClick={()=>signOut(auth)}>salir</a></li>
+            <li className="nav-item"><a className="nav-link" onClick={() => signOut(auth)}>salir</a></li>
             <li className="nav-item">
               <a
                 className="nav-link p-0 fs-4 "
-                data-bs-toggle="modal"
-                data-bs-target="#exampleModal">
+                onClick={handleShow}>
                 <FontAwesomeIcon className="iconosNav" icon={["far", "user"]} />
                 <span className="ms-2 opcionesNav">Mi cuenta</span>
               </a>
@@ -68,7 +81,25 @@ export default function Navbar() {
           </ul>
         </div>
       </div>
-      <Login />
+      <Modal show={show} onHide={handleClose} backdrop="static" keyboard={false}>
+        <Login handleClose={handleClose} handleShow={handleShow} />
+        {/* <Modal.Header closeButton>
+          <Modal.Title id="example-custom-modal-styling-title">
+            Custom Modal Styling
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <p>
+            Ipsum molestiae natus adipisci modi eligendi? Debitis amet quae unde
+            commodi aspernatur enim, consectetur. Cumque deleniti temporibus
+            ipsam atque a dolores quisquam quisquam adipisci possimus
+            laboriosam. Quibusdam facilis doloribus debitis! Sit quasi quod
+            accusamus eos quod. Ab quos consequuntur eaque quo rem! Mollitia
+            reiciendis porro quo magni incidunt dolore amet atque facilis ipsum
+            deleniti rem!
+          </p>
+        </Modal.Body> */}
+      </Modal>
     </nav>
   )
 }
